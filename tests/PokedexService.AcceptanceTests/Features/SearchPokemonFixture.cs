af751140aaed1c7fd51debda7c03d2bb7fd7b0f9
Tai Fa Tang <taifatang@hotmail.com>
2021-11-15 00:08:57 +0000

    Add pokemon search acceptance tests

---
+++ tests/PokedexService.AcceptanceTests/Features/SearchPokemonFixture.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PokedexService.AcceptanceHelper.Contracts;
+using TestStack.BDDfy;
+
+namespace PokedexService.AcceptanceTests.Features
+{
+    [Story(AsA = "pokemon enthusiast",
+        IWant = "search for pokemon",
+        SoThat = "I can understand the pokemon more")]
+    [TestFixture]
+    public class SearchPokemonFixture
+    {
+        private HttpClient apiClient;
+        
+        private string _pokemon;
+        private HttpResponseMessage _httpResponseMessage;
+        
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.test.json")
+                .Build();
+
+            apiClient = new HttpClient()
+            {
+                BaseAddress = new Uri(builder["Service:BaseUrl"])
+            };
+        }
+
+        [Test]
+        public void Search_pokemon()
+        {
+            this.When(_ => _.IAmLookingFor("mewTwo"))
+                .Then(_ => _.ISearchPokemon())
+                .Then(_ => _.AHttpStatusCodeIsReturned(HttpStatusCode.OK))
+                .And(_ => _.APokemonIsReturned())
+                .BDDfy();
+        }
+
+        [Given]
+        private void IAmLookingFor(string pokemon)
+        {
+            _pokemon = pokemon;
+        }
+
+        [When]
+        private async Task ISearchPokemon()
+        {
+            _httpResponseMessage = await apiClient.GetAsync($"pokemon/{_pokemon}");
+        }
+
+        [Then]
+        private async Task APokemonIsReturned()
+        {
+            var responseBody = await _httpResponseMessage.Content.ReadAsStringAsync();
+            var searchResult = JsonConvert.DeserializeObject<PokemonSearchResponse>(responseBody);
+
+            searchResult.Should().NotBeNull();
+            searchResult.Name.Should().Be(_pokemon);
+        }
+
+        [Then]
+        private void AHttpStatusCodeIsReturned(HttpStatusCode httpStatusCode)
+        {
+            _httpResponseMessage.StatusCode.Should().Be(httpStatusCode);
+        }
+    }
+}
